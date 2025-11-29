eec36f675ebd12c1009faef96aa436627943a4df
Austin Bradford <willsk84p1e@gmail.com>
2025-11-28 20:31:39 -0700

    setup the simplest possible variant of a react router project with the declarative mode (and) while leveraging vite

---
+++ app/frontend/README.md
@@ -0,0 +1,7 @@
+## A few things to know:
+
+build / run
+```
+npx react-router build
+npx react-router-serve build/server/index.js
+```
--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -3,4 +3,16 @@ import { defineConfig } from "vite";
 
 export default defineConfig({
     plugins: [reactRouter()],
+    server: {
+        host: '0.0.0.0',
+        port: 8080,
+        watch: {
+            usePolling: true,
+            interval: 100
+        },
+        allowedHosts: [
+            "host.docker.internal",
+            "localhost",
+        ]
+    }
 });
