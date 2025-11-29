a85a1d277200b8f32669952c70287714e5077cb2
Austin Bradford <willsk84p1e@gmail.com>
2025-11-29 14:23:02 -0700

    setting up some sort of preliminary coffee theme (loosely)

--- app/frontend/src/api/resource.js
+++ app/frontend/src/api/resource.js
@@ -2,35 +2,31 @@ import apiClient from "./client"
 
 /***
  * Works to be the default implementation of how we connect to the roasting API.
- * Since js doesn't have abstract base classes we'll throw a little extra coding in here to force implementation.
+ * Since js doesn't have abstract base classes we'll throw a little extra coding 
+ * in here to force implementation.
  */
 export class Resource {
 
     client = apiClient
-
-    constructor() {
-
-    }
 
     get = async () => {
         throw Error('get must be implemented')
     }
 
     list = async () => {
-        throw Error('get must be implemented')
+        throw Error('list must be implemented')
     }
 
     create = async () => {
-        throw Error('get must be implemented')
+        throw Error('create must be implemented')
     }
 
     delete = async () => {
-        throw Error('get must be implemented')
+        throw Error('delete must be implemented')
     }
 
-    // TODO I don't care to implement PUT
     partialUpdate = async () => {
-        throw Error('get must be implemented')
+        throw Error('partialUpdate must be implemented')
     }
 
 }
