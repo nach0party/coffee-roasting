8416fc1afec704937ac706dc2f29f1f234e3f77c
Austin Bradford <willsk84p1e@gmail.com>
2025-11-29 08:16:58 -0700

    decided to switch to data variant of router cause apparently declarative is outdated and was having some funky issues with it

---
+++ app/frontend/src/main.jsx
@@ -0,0 +1,14 @@
+import { createRoot } from 'react-dom/client'
+import { createBrowserRouter } from 'react-router'
+import { RouterProvider } from 'react-router/dom'
+import './index.css'
+import App from './App.jsx'
+
+const router = createBrowserRouter([{
+  path: '/',
+  element: <App />
+}])
+
+createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
--- app/frontend/vite.config.js
+++ app/frontend/vite.config.js
@@ -1,18 +1,7 @@
-import { reactRouter } from "@react-router/dev/vite";
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vite.dev/config/
 export default defineConfig({
-    plugins: [reactRouter()],
-    server: {
-        host: '0.0.0.0',
-        port: 8080,
-        watch: {
-            usePolling: true,
-            interval: 100
-        },
-        allowedHosts: [
-            "host.docker.internal",
-            "localhost",
-        ]
-    }
-});
+  plugins: [react()],
+})
