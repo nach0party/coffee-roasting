5bffc7e8abad898725b8f046a225ccc6854f5cb3
Austin Bradford <willsk84p1e@gmail.com>
2026-01-19 20:38:49 -0700

    removing some not needed files

--- app/backend/app/roast/management/commands/test_migration.py
+++
@@ -1,9 +0,0 @@
-from django.core.management.base import BaseCommand
-from pycountry import countries
-
-
-class Command(BaseCommand):
-
-    def handle(self, *args, **options) -> None:
-        country = countries.get(name="United States")
-        print(country, "country")
